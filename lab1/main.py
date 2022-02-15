ad5aa6c6e96d35196be82e7b9dd61b4dae30e2ad
Vector <hiuon.x@gmail.com>
2022-02-16 00:06:53 +0300

    Update code

--- lab1/main.py
+++ lab1/main.py
@@ -1,23 +1,30 @@
 from PIL import Image
+from math import *
 
 #constants
 plus_value = -100
-multiply_value = 2
-r_neg = 100
-r_log = 2
+multiply_value = 1.9
+gamma_const = 5
+c_const_log = 10
+c_const = 1
+
 
 #lists of pixels
 lst_plus = []
 lst_multiply = []
 lst_neg = []
 lst_log = []
+lst_step = []
 
-#import picture and convert it to L mode (only one component)
+#import picture1 and convert it to L mode (only one component)
 img = Image.open("./image.jpg")
 img_data = img.convert("L").getdata()
 
+#import picture2 and convert it to L mode (only one component)
+img2 = Image.open("./image2.jpg")
+img2_data = img2.convert("L").getdata()
 
-
+#add constant
 for i in img_data:
     if i + plus_value > 255:
         lst_plus.append(255)
@@ -26,6 +33,43 @@ for i in img_data:
     else:
         lst_plus.append(i + plus_value)
 
-img1 = Image.new("L", img.size)
-img1.putdata(lst_plus)
-img1.save("./image_new.jpg")
+#mult constant
+for i in img_data:
+    if i * multiply_value > 255:
+        lst_multiply.append(255)
+    elif i * multiply_value < 0:
+        lst_multiply.append(0)
+    else:
+        lst_multiply.append(int(i * multiply_value))
+
+#neg
+for i in img_data:
+    lst_neg.append(255 - 1 - i)
+
+#log
+for i in img2_data:
+    lst_log.append(int(c_const_log*log10(1 + i)))
+
+#step
+for i in img2_data:
+    lst_step.append(int(c_const*pow(i, gamma_const)))
+
+img_plus = Image.new("L", img.size)
+img_plus.putdata(lst_plus)
+img_plus.save("./image_plus.jpg")
+
+img_mult = Image.new("L", img.size)
+img_mult.putdata(lst_multiply)
+img_mult.save("./image_mult.jpg")
+
+img_neg = Image.new("L", img.size)
+img_neg.putdata(lst_neg)
+img_neg.save("./image_neg.jpg")
+
+img_log = Image.new("L", img2.size)
+img_log.putdata(lst_log)
+img_log.save("./image_log.jpg")
+
+img_step = Image.new("L", img2.size)
+img_step.putdata(lst_step)
+img_step.save("./image_step.jpg")
